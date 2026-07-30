b3521c01876d5db1ea95a4031d2244e423dfa224
Prequena-84 <pedrorequenarondon@hotmail.com>
2026-07-30 08:15:57 -0400

    [FIX]-change_repository

--- src/modules/transaction/repositories/transaction.repository.ts
+++ src/modules/transaction/repositories/transaction.repository.ts
@@ -11,7 +11,7 @@ export class TransactionRepository {
     constructor(
         @InjectRepository(TransactionEntity)
         private readonly transactionRepository: Repository<TransactionEntity>
-    ) {}
+    ) {};
 
     // 1. Crear Transacción (Inicia en PENDING)
     async createTransaction(data: TransactionCreateDTO): Promise<TransactionEntity> {
@@ -20,7 +20,7 @@ export class TransactionRepository {
             saga_state: SagaStateEnum.PENDING,
         });
         return this.transactionRepository.save(newTransaction);
-    }
+    };
 
     // 2. Buscar Transacción por ID
     async findById(transactionId: string): Promise<TransactionEntity> {
@@ -29,17 +29,14 @@ export class TransactionRepository {
         });
         if (!transaction) throw new NotFoundException(`No se encontró la transacción ${transactionId}`);
         return transaction;
-    }
+    };
 
     // 3. Actualizar Estado de la Saga
     async updateSagaState(transactionId: string, stateData: TransactionStateDTO): Promise<TransactionEntity> {
         const transaction = await this.findById(transactionId);
-        
         transaction.saga_state = stateData.saga_state;
-        if (stateData.failure_reason) {
-            transaction.failure_reason = stateData.failure_reason;
-        }
+        if (stateData.failure_reason) transaction.failure_reason = stateData.failure_reason;
 
         return this.transactionRepository.save(transaction);
-    }
-}
+    };
+};
